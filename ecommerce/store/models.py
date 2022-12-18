14e7edb21e09a55bcfa1f351c88cc89012b8aeb0
OVERDOZZZE <saparbekov113@gmail.com>
2022-12-18 17:49:11 +0600

    Добавлены модели и подключена база данных
    Сделано:
    
    1. В главном приложении в models.py прописаны следующие классы:
    	1.1 class Customer, который является дочерним классом имеющегося класса джанго User, который добавляет
    	новые поля имя и email пользователя
    	1.2 class Product содержит информацию о продукте с полями "название", "цена" и "digital", который
    	является BooleanField и отвечает за наличие продукта на складе
    	1.3 class Order содержащий информацию о заказе
    	1.4 class OrderItem, который содержит информацию о товаре, на который будет производиться заказ,
    	такие как: сам продукт, заказ, количество, и дата
    	1.5 class ShippingAddress содержит информацию об адресе пользователя
    
    2. Проведено подкючение к базе данных при помощи миграций
    
    3. Добавлены магические методы к каждому классу (__str__)

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,81 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, null=True)
+    email = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=False)
+    #image
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    date_orderd = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    name = models.CharField(max_length=255, null=True)
+    transaction_id = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=255, null=True)
+    city = models.CharField(max_length=255, null=True)
+    state = models.CharField(max_length=255, null=True)
+    zipcode = models.CharField(max_length=255, null=True)
+    day_added = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return self.address
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
